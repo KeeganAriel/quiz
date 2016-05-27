cf522b1bf222c4ddd29b630b46a8bbaaa7083e6f
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-27 09:33:51 -0700

    fix formatting/ inline

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Question = function(question, answers, rightAnswer){
 
 Question.prototype.getOptions = function() {
 	var options = [this.rightAnswer].concat(this.answers);
-		return _.shuffle(options);
+	return _.shuffle(options);
 };
 
 Question.prototype.display = function() {
@@ -43,43 +43,36 @@ Quiz.prototype.currentQuestion = function() {
 };
 
 
-// create var to store user choice
-	// get answer and check that it's right
-	// track checked
-// update count
-// track amout right
-
-
 var matrixQuestion = new Question (
 	"In The Wachowski’s Matrix trilogy, what is Neo’s given name?",
 	["John B. Thompson", "Andy N. Johnson", "Bill B. Anderton"],
 	'Thomas A. Anderson' 
 
-);
+	);
 
 var hackersQuestion = new Question (
 	"In the Angelina Jolie’s first major motion picture, Hackers, who says ‘Spandex: it's a privilege, not a right.'?",
 	['The Plague', 'Dade Murphy', 'Hero Protagonist', ],
 	'Cereal Killer'
-);
+	);
 
 var jnQuestion = new Question (
 	"Who wrote the short story that the 1995 cyberpunk movie Johnny Mnemonic, staring the totally awesome Keanu Reeves, is based on?",
 	['Neal Stephenson', 'Bruce Sterling', 'Richard K. Morgan'],
 	'William Gibson'
-);
+	);
 
 var netQuestion = new Question (
 	"Angela receives a copy of this band's website by floppy disk. What is the name of the band whose website is the backdoor to the entire internet?",
 	["Bach’s Babes", "Beethoven’s Phantom", "Schubert’s Spooks"],
 	"Mozart's Ghost" 
-);
+	);
 
 var existenzQuestion = new Question (
 	"In the 1995 movie eXistenZ what kind of projectile does Ted pull from Allegra's arm after she’s been shot?",
 	["A bullet", "A fingernail", "A bottle cap"],
 	"A tooth"
-);
+	);
 
 var questions = [existenzQuestion, netQuestion, jnQuestion, hackersQuestion, matrixQuestion];
 
@@ -94,12 +87,13 @@ $(document).ready(function() {
 		$('.question').show();
 		$('.quizEnd').hide();
 		$('.bottom').show();
+		$('.socialIcons').hide();
 
 		quiz.currentQuestion().display();
 
 	});
 
- 
+	
 	$('.submitButton').click(function() {
 		event.preventDefault();
 		var userAnswer = $('input[name=answer]:checked').val();
@@ -112,40 +106,22 @@ $(document).ready(function() {
 
 
 		quiz.currentQuestionIndex++;
-			if (quiz.currentQuestionIndex <= 4) {
-				quiz.currentQuestion().display();
-			} else {
-				$('.question').hide();
-				$('.quizEnd').show();
-				$('#rightTotal').text(quiz.score);
-				$('.bottom').hide();
-
-			}
-
+		if (quiz.currentQuestionIndex <= 4) {
+			quiz.currentQuestion().display();
+		} else {
+			$('.question').hide();
+			$('.quizEnd').show();
+			$('.socialIcons').show();
+			$('#rightTotal').text(quiz.score);
+			$('.bottom').hide();
 
 
+		}
 
-
-		// if rightAnswer is :checked
-			// then add to right count
-
-
-		// when you click the submit buttion
-		// get user imput from radio button`
-			// get selected radio button value
-		// compare to right answer 
-		// add to counter
-		// add to right answer count
-		// clear radio input
-		// random next question
 	});
 
 
-var newGame = function() {
-	// reload game
-	// set right to 0 of X
-	// reset correct answers
-	// display start page
+	var newGame = function() {
 	$('.question').hide();
 	$('.quizStart').show();
 	$('.quizEnd').hide();
@@ -154,7 +130,7 @@ var newGame = function() {
 	$('#count').text(0);
 	quiz = new Quiz(questions);
 
-	};
+};
 
 var aboutMe = function() {
 	$('.aboutInfo').alert('test');
@@ -168,5 +144,5 @@ $("a.new").click(function() {
 $("a.about").click(function() {
 	$('.aboutInfo').dialog();
 });
- 
+
 });
